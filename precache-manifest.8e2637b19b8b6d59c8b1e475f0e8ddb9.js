5e1264eaec7c0ee2f981bfcf7671515469c38932
rolyli <leem72104@gmail.com>
2021-09-11 22:50:52 +0100

    Port from Netlify manual deploy

---
+++ precache-manifest.8e2637b19b8b6d59c8b1e475f0e8ddb9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b434ba1f3709596002271f5a9f5f501",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8f6647ae8680f4b3444",
+    "url": "/static/css/2.d3b42f54.chunk.css"
+  },
+  {
+    "revision": "7e0892d3ce30439c4dfb",
+    "url": "/static/css/main.d59c5523.chunk.css"
+  },
+  {
+    "revision": "d8f6647ae8680f4b3444",
+    "url": "/static/js/2.ede42008.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.ede42008.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e0892d3ce30439c4dfb",
+    "url": "/static/js/main.df5c4975.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
